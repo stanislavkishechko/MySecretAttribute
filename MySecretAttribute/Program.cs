814a82a67ef68c0df16b958e3e917c83963a97c9
Stanislav <Stanislav@DESKTOP-L3M7Q2B>
2022-12-21 21:40:23 +0200

    Добавьте файлы проекта.

---
+++ MySecretAttribute/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MySecretAttribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sw = new StreamWriter("Record Log.txt"))
+            {
+                var type = typeof(Product);
+
+                var properties = type.GetProperties();
+
+                var record = type.Name;
+
+                sw.WriteLine("Class: {0}\n", record);
+
+                foreach (var prop in properties)
+                {
+                    var attrs = prop.GetCustomAttributes(false);
+
+                    if (!attrs.Any(a => a.GetType() == typeof(SecretAttribute)))
+                    {
+                        sw.WriteLine($"{prop.Name}");
+                    }
+                }
+            }
+
+            using (var sr = new StreamReader("Record Log.txt"))
+            {
+                var text = sr.ReadToEnd();
+                Console.Write(text);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MySecretAttribute/SecretAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecretAttribute
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+    public class SecretAttribute : Attribute
+    {       
+        public SecretAttribute() { }
+    }
+}
